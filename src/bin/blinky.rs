9a83ad4c9d0d1eccf129603948dead4fbb8449d2
Adam Hott <24814301+CodingInGreen@users.noreply.github.com>
2024-06-24 11:45:21 +0100

    Modifiy to use TIM1_CH1

--- src/bin/blinky.rs
+++ src/bin/blinky.rs
@@ -4,7 +4,7 @@
 use defmt::*;
 use embassy_executor::Spawner;
 use embassy_stm32::gpio::OutputType;
-use embassy_stm32::time::khz;
+use embassy_stm32::time::hz;
 use embassy_stm32::timer::simple_pwm::{PwmPin, SimplePwm};
 use embassy_stm32::timer::Channel;
 use embassy_stm32::Config;
@@ -37,22 +37,27 @@ async fn main(_spawner: Spawner) {
     let p = embassy_stm32::init(config);
     info!("Hello World!");
 
-    let ch1 = PwmPin::new_ch1(p.PA6, OutputType::PushPull);
-    let mut pwm = SimplePwm::new(p.TIM3, Some(ch1), None, None, None, khz(10), Default::default());
-    let max = pwm.get_max_duty();
+    let ch1 = PwmPin::new_ch1(p.PA8, OutputType::PushPull);
+    let mut pwm = SimplePwm::new(p.TIM1, Some(ch1), None, None, None, hz(50), Default::default());
+    let max_duty = pwm.get_max_duty();
+    let min_pulse = 500; // Minimum pulse width in microseconds
+    let max_pulse = 2500; // Maximum pulse width in microseconds
+
     pwm.enable(Channel::Ch1);
 
     info!("PWM initialized");
-    info!("PWM max duty {}", max);
 
     loop {
-        pwm.set_duty(Channel::Ch1, 0);
-        Timer::after_millis(300).await;
-        pwm.set_duty(Channel::Ch1, max / 4);
-        Timer::after_millis(300).await;
-        pwm.set_duty(Channel::Ch1, max / 2);
-        Timer::after_millis(300).await;
-        pwm.set_duty(Channel::Ch1, max - 1);
-        Timer::after_millis(300).await;
+        // Set duty cycle for min pulse width (500µs)
+        pwm.set_duty(Channel::Ch1, (min_pulse * max_duty) / 20_000);
+        Timer::after_millis(1000).await;
+
+        // Set duty cycle for mid pulse width (1500µs)
+        pwm.set_duty(Channel::Ch1, (1500 * max_duty) / 20_000);
+        Timer::after_millis(1000).await;
+
+        // Set duty cycle for max pulse width (2500µs)
+        pwm.set_duty(Channel::Ch1, (max_pulse * max_duty) / 20_000);
+        Timer::after_millis(1000).await;
     }
 }
